bf6868be6b630ee8b373071593f9739d59a9959a
birugh <notblue.https@gmail.com>
2025-06-27 15:46:27 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        backgroundColor: Color(0xFF892de1),
+        body: Center(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/quiz-logo.png', width: 250),
+                SizedBox(height: 50),
+                Text(
+                  'Learn Flutter the fun way!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: 20),
+                OutlinedButton(
+                  onPressed: () {
+                    //
+                  },
+                  child: Text(
+                    'Start Quiz',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    side: WidgetStateProperty.all(
+                      BorderSide(width: 2, color: Colors.white),
+                    ),
+                    textStyle: WidgetStateProperty.all(
+                      TextStyle(color: Colors.white),
+                    ),
+                    shape: WidgetStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
